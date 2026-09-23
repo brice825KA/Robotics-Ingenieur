06ed7c25436b9c557fb6f4a0c430be9087405675
brice825KA <brice825KA@users.noreply.github.com>
2026-09-23 22:35:05 +0100

    add dir Attribute Parser and exeptional Server in Learn CPP

---
+++ Robotics Ingenieur/Learn CPP/Attribute Parser/include/header.hpp
@@ -0,0 +1,13 @@
+#ifndef _HEADER_HPP_
+    #define _HEADER_HPP_
+
+    #include <cmath>
+    #include <cstdio>
+    #include <vector>
+    #include <iostream>
+    #include <algorithm>
+    using namespace std;
+
+vector<char *> parsing_data(string line, char *limits);    
+
+#endif
---
+++ Robotics Ingenieur/Learn CPP/Attribute Parser/main.cpp
@@ -0,0 +1,12 @@
+#include "include/header.hpp"
+
+vector<char *> parsing_data(string line, char *limits) {
+    vector<char *> parsing;
+    char *buffer = NULL;
+
+    return parsing;
+}
+
+int main(int argv, char **args) {
+    return 0;
+}
