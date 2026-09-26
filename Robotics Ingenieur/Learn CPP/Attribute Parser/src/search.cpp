01217d4873834e8f05578e1daa4daa199838ccc9
brice825KA <brice825KA@users.noreply.github.com>
2026-09-26 17:08:56 +0100

    Nothing to do

--- Robotics Ingenieur/Learn CPP/Attribute Parser/src/search.cpp
+++ Robotics Ingenieur/Learn CPP/Attribute Parser/src/search.cpp
@@ -1,17 +1,31 @@
 #include "../include/header.hpp"
 #include <cstdio>
+#include <cstdlib>
 
 int search_in_stock(vector<parse_tag_t> data, string request) {
     vector<char *> parse = parsing_data(request, ".~");
 
-    if (parse.empty()) {
+    if (parse.size() < 2) { // Une requête valide nécessite au moins un tag et un attribut
         printf("Not Found!\n");
+        for (char *ptr : parse) free(ptr);
         return 1;
     }
+
+    // Réassembler la hiérarchie demandée par la requête (ex: "tag1.tag2")
+    string requested_path = "";
+    for (size_t i = 0; i < parse.size() - 1; i += 1) {
+        requested_path += parse[i];
+        if (i < parse.size() - 2) requested_path += ".";
+    }
+    
+    char *attr_target = parse.back(); // Le dernier élément est l'attribut
+
     for (int i = 0; i < data.size(); i += 1) {
-        if (strcmp(parse[0], data[i].name_tag) == 0) {
-            for (int j = 1; j < data[i].table.size(); j+= 1) {
-                if (strcmp(parse.back(),data[i].table[j]) == 0) {
+        // Comparaison avec le chemin absolu reconstruit
+        if (strcmp(requested_path.c_str(), data[i].name_tag) == 0) {
+            // Recherche de l'attribut dans la table
+            for (int j = 1; j < data[i].table.size(); j += 1) {
+                if (strcmp(attr_target, data[i].table[j]) == 0) {
                     if (j + 1 < data[i].table.size()) {
                         cout << data[i].table[j + 1] << endl;
                         for (char *ptr : parse) free(ptr);
@@ -21,7 +35,8 @@ int search_in_stock(vector<parse_tag_t> data, string request) {
             }
         }
     }
-    for (char *ptr: parse) free(ptr);
+
+    for (char *ptr : parse) free(ptr);
     printf("Not Found!\n");
     return 1;
 }
--- Robotics Ingenieur/Learn CPP/Attribute Parser/src/stock.cpp
+++ Robotics Ingenieur/Learn CPP/Attribute Parser/src/stock.cpp
@@ -1,18 +1,38 @@
 #include "../include/header.hpp"
+#include <cstdlib>
 
-void stock_data(vector<parse_tag_t>&data, int query, char *limits) {
+void stock_data(vector<parse_tag_t>& data, int query, char *limits) {
     string d;
     vector<char *> parsing;
-    parse_tag_t parse;
+    vector<string> current_path; // Permet de suivre l'imbrication actuelle
 
     for (int i = 0; i < query; i += 1) {
         getline(cin, d);
         parsing = parsing_data(d, limits);
-        if (parsing[0][0] == '/' || parsing.empty()) {
+        
+        if (parsing.empty()) continue;
+
+        // Si c'est une balise fermante (ex: </tag1>)
+        if (parsing[0][0] == '/') {
+            if (!current_path.empty()) {
+                current_path.pop_back(); // On sort du tag actuel
+            }
             for (char *ptr : parsing) free(ptr);
             continue;
         }
-        parse.name_tag = (parsing[0]);
+
+        // Ajouter le tag actuel à la hiérarchie imbriquée
+        current_path.push_back(parsing[0]);
+
+        // Construire le nom complet sous forme de chemin (ex: tag1.tag2)
+        string full_path = "";
+        for (size_t k = 0; k < current_path.size(); k++) {
+            full_path += current_path[k];
+            if (k < current_path.size() - 1) full_path += ".";
+        }
+
+        parse_tag_t parse;
+        parse.name_tag = strdup(full_path.c_str()); // Stockage du chemin absolu du tag
         parse.table = parsing;
         data.emplace_back(parse);
     }   
